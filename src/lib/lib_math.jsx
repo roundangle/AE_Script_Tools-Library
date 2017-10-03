6f4ce9405d624e440ff873c773436379471b0697
Andrew Hildebrand <roundAngleDesign@gmail.com>
2017-10-02 22:55:08 -0400

    Combined Random Math into lib_Math

--- src/lib/lib_math.jsx
+++ src/lib/lib_math.jsx
@@ -1,14 +1,60 @@
-// $.writeln(rad_to_deg(Math.PI));
-function rad_to_deg (inRadians) {  return inRadians * 180 / Math.PI; }
+var MathGeneral = (function(){
+  function rad_to_deg (inRadians) {  return inRadians * 180 / Math.PI; }
 
-// $.writeln(deg_to_rad(180));
-function deg_to_rad (inDegrees) {  return inDegrees * Math.PI / 180; }
+  // $.writeln(deg_to_rad(180));
+  function deg_to_rad (inDegrees) {  return inDegrees * Math.PI / 180; }
 
-// averageAryNums([1, 2, 3, 4, 5, 6, 7]);
-function averageAryNums(inAry){
-  var outVal=0;
-      for(var i=0; i<inAry.length; i++){
-          outVal += inAry[i];
+  // averageAryNums([1, 2, 3, 4, 5, 6, 7]);
+  function averageAryNums(inAry){
+    var outVal=0;
+        for(var i=0; i<inAry.length; i++){
+            outVal += inAry[i];
+        }
+    return outVal/inAry.length;
+  }
+
+  return {    // Public Functions
+    rad_to_deg: rad_to_deg,
+    deg_to_rad: deg_to_rad,
+    averageAryNums: averageAryNums
+  };
+})();
+
+var Random = (function(){
+  function minMax(min, max, returnIntBool){
+      var dif = max - min;
+      var thisRan = Math.random();
+      thisRan *= dif;
+      thisRan += min;
+      if (returnIntBool == true){
+        thisRan = Math.floor(thisRan);
       }
-  return outVal/inAry.length;
-}
+      return thisRan;
+  }
+  function range(inVal){
+      var thisRan = Math.random();
+      thisRan -= .5;
+      thisRan *= Math.abs(inVal);
+      return thisRan;
+  }
+  function rangeAry(inAry){
+      var outAry = [];
+      for(var i=0; i<inAry.length; i++){
+          outAry[i] = range(inAry[i]);
+      }
+      return outAry;
+  }
+  function aryLngth(aryLngth){
+      var outAry = [];
+      for(var i = 0; i < aryLngth; i++){
+          outAry [i] = Math.random();
+      }
+      return outAry;
+  }
+  return {    // Public Functions
+    minMax: minMax,
+    range:  range,
+    rangeAry: rangeAry,
+    aryLngth: aryLngth
+  };
+})();
